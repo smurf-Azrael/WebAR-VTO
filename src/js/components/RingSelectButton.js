dc4e0130f93e3af6ac71049e4b621d917d126a94
smurf-Azrael <luisvelasco00117@gmail.com>
2023-05-23 18:05:00 -0700

    update:ui

--- src/js/components/RingSelectButton.js
+++ src/js/components/RingSelectButton.js
@@ -1,33 +1,7 @@
 import React from 'react'
 import { styled } from '@mui/material/styles';
 import IconButton from '@mui/material/IconButton';
-import ringPath1 from '../../assets/img/rings/1.jpg';
-import ringPath2 from '../../assets/img/rings/2.jpg';
-import ringPath3 from '../../assets/img/rings/3.jpg';
 import { Box } from '@mui/material';
-
-const images = [
-  {
-    id: 0,
-    url: ringPath1,
-    title: 'Model 1',
-    width: '33.33%',
-  },
-  {
-    id: 1,
-    url: ringPath2,
-    title: 'Model 2',
-    width: '33.33%',
-  },
-  {
-    id: 2,
-    url: ringPath3,
-    title: 'Model 3',
-    width: '33.33%',
-  },
-];
-
-
 
 const RoundButton = styled(IconButton)(({ theme }) => ({
     margin: 20,
@@ -39,6 +13,7 @@ const RoundButton = styled(IconButton)(({ theme }) => ({
   }));
 
 export default function RingSelectButton(props) {
+  const { images } = props;
   return (
     <Box sx={{
             display: 'flex',
